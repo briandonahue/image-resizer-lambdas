ffe1fccfc887142ef628ab2287dc8f86e493640d
Brian Donahue <brian.donahue@vitreosolutions.com>
2016-03-26 15:06:03 -0400

    Fetches s3 file

--- resizer/lib/index.js
+++ resizer/lib/index.js
@@ -1,7 +1,29 @@
 import aws from 'aws-sdk';
 
 
-export function handler () {
-  console.log("Hello world!");
-}
+export function handler(event, context) {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  const s3 = new aws.S3();
 
+  const eventInfo = {
+    bucket,
+    key
+  };
+
+  new Promise((resolve, reject) => {
+    s3.getObject({
+      Bucket: eventInfo.bucket,
+      Key: eventInfo.key
+    }, function(err, data) {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  }).then((data) => {
+      console.log(data);
+    },
+    (err) => {
+      console.log("ERROR:");
+      console.log(err);
+    });
+}
