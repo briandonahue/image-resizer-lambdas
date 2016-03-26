8ed96c4bd26fa9c041d3a1b5631633adfe44891c
Brian Donahue <brian.donahue@vitreosolutions.com>
2016-03-26 17:08:41 -0400

    refactored

--- resizer/lib/index.js
+++ resizer/lib/index.js
@@ -5,53 +5,14 @@ const gm = _gm.subClass({ imageMagick: true }); // Enable ImageMagick integratio
 const s3 = new aws.S3();
 
 export function handler(event, context) {
-  const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  const eventInfo = parseEvent(event);
 
-  const eventInfo = {
-    bucket,
-    key
-  };
-
-  new Promise((resolve, reject) => {
-      s3.getObject({
-        Bucket: eventInfo.bucket,
-        Key: eventInfo.key
-      }, function(err, data) {
-        if (err) reject(err);
-        else resolve(data);
-      });
-    })
+  getObject(eventInfo.bucket, eventInfo.key)
   .then((data) => {
-    var srcBuffer = data.Body;
-    var width = 300;
-    var height = 200;
-    var quality = 65;
-    var imageType = 'jpg';
-
-    return new Promise((resolve, reject) => {
-      gm(srcBuffer)
-        .resize(width, height, "!")
-        .quality(quality)
-        .toBuffer(imageType, (err, buffer) => {
-          if (err) reject(err);
-          else {
-            resolve(buffer);
-          }
-        });
-    });
+    return resizeImage(data.Body, 300, 200);
   })
   .then((resizedBuffer) => {
-    return new Promise((resolve, reject) => {
-      s3.putObject({
-        Bucket: "sir-test-bucket-resized",
-        Key: "test.jpg",
-        Body: resizedBuffer
-      }, function(err, response) {
-        if (err) reject(err);
-        else resolve(response);
-      });
-    });
+    return putObject("sir-test-bucket-resized", eventInfo.key, resizedBuffer);
   })
   .then((result) => {
     console.log(result);
@@ -61,3 +22,55 @@ export function handler(event, context) {
     console.log(err);
   });
 }
+
+const parseEvent = (event) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+
+  return  { bucket, key };
+}
+
+const getObject = (bucket, key) => {
+  return new Promise((resolve, reject) => {
+      s3.getObject({
+        Bucket: bucket,
+        Key: key
+      }, function(err, data) {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    })
+
+}
+
+const resizeImage = (srcBuffer, width, height, quality, imageType) => {
+  var quality = quality || 65;
+  var imageType = imageType || 'jpg';
+
+  return new Promise((resolve, reject) => {
+    gm(srcBuffer)
+      .resize(width, height, "!")
+      .quality(quality)
+      .toBuffer(imageType, (err, buffer) => {
+        if (err) reject(err);
+        else {
+          resolve(buffer);
+        }
+      });
+  });
+}
+
+const putObject = (bucket, key, body, metadata) => {
+  return new Promise((resolve, reject) => {
+    s3.putObject({
+      Bucket: bucket,
+      Key: key,
+      Body: body,
+      Metadata: metadata
+    }, function(err, response) {
+      if (err) reject(err);
+      else resolve(response);
+    });
+  });
+}
+
